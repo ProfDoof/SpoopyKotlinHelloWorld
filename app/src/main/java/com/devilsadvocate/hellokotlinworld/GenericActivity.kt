815fa727ef375b5d7b002a3c98bb3e2709a5a780
fireflamemm <jmm15f@acu.edu>
2019-10-27 09:45:55 -0500

    Welp, this is the final commit finished the spoopiness

--- app/src/main/java/com/devilsadvocate/hellokotlinworld/GenericActivity.kt
+++ app/src/main/java/com/devilsadvocate/hellokotlinworld/GenericActivity.kt
@@ -1,12 +1,19 @@
 package com.devilsadvocate.hellokotlinworld
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_generic.*
 
 class GenericActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generic)
+
+        val mediaPlayer = MediaPlayer.create(applicationContext, R.raw.generic_sound)
+
+        mediaPlayer?.start()
+        genericImageView.setImageResource(R.drawable.generic_photo)
     }
 }
